329f2ede22566846984917f9fcd9abdb0ec11f61
SieunKiim <sieun960513@naver.com>
2024-05-21 21:20:32 +0900

    init

---
+++ src/main/java/com/eod/sitree/SitreeApplication.java
@@ -0,0 +1,13 @@
+package com.eod.sitree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eod/sitree/SitreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eod.sitree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

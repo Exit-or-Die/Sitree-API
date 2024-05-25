3f713768f9a8cb79f30e160816bee229441d1628
SieunKiim <sieun960513@naver.com>
2024-05-26 00:40:25 +0900

    cd: deploy 테스트2

--- src/main/java/com/eod/sitree/health/HealthCheckController.java
+++ src/main/java/com/eod/sitree/health/HealthCheckController.java
@@ -1,9 +1,11 @@
 package com.eod.sitree.health;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/health")
+@RestController
+@RequestMapping("/health")
 public class HealthCheckController {
 
     @GetMapping

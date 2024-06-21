85a28cb036566358653de42ef343c6c99641626d
JackCokebb <reo2726@gmail.com>
2024-06-21 19:50:57 +0900

    refactor: 정리

--- src/main/java/com/eod/sitree/auth/service/AuthService.java
+++ src/main/java/com/eod/sitree/auth/service/AuthService.java
@@ -1,39 +1,20 @@
 package com.eod.sitree.auth.service;
 
-import com.eod.sitree.auth.domain.MemberClaim;
-import com.eod.sitree.common.exception.BadRequestException;
-import com.eod.sitree.common.exception.UnauthorizedException;
-import com.eod.sitree.auth.ui.dto.SingleTokenDto;
-import com.eod.sitree.auth.ui.dto.TokenDto;
-import com.eod.sitree.common.exception.ApplicationErrorType;
-import com.eod.sitree.member.domain.model.Member;
 import com.eod.sitree.member.domain.modelrepository.MemberRepository;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.micrometer.common.util.StringUtils;
-import jakarta.servlet.http.HttpServletRequest;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.util.Date;
-import java.util.Optional;
 import lombok.Getter;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
-
-    private final MemberRepository memberRepository;
 
     @Getter
     private final KeyPair jwtKeypair;
 
     public AuthService(MemberRepository memberRepository) {
 
-        this.memberRepository = memberRepository;
         this.jwtKeypair = generateKeyPair();
     }
 

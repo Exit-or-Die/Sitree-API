78ceb19f61e513430d83a55139e8d28aac7816ed
SieunKiim <sieun960513@naver.com>
2024-07-28 16:30:24 +0900

    fix: tagEntity 유니크 값 수정

--- src/main/java/com/eod/sitree/project/infra/entity/TagEntity.java
+++ src/main/java/com/eod/sitree/project/infra/entity/TagEntity.java
@@ -2,12 +2,10 @@ package com.eod.sitree.project.infra.entity;
 
 import com.eod.sitree.project.domain.model.Tag;
 import jakarta.persistence.Column;
-import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
 
@@ -20,9 +18,10 @@ public class TagEntity {
     @Column(name = "id")
     private Long tagId;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private Long projectId;
 
     public TagEntity(Long projectId, Tag tag) {

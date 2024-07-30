107fbf22deb6636a3b6d732a1e2ab90cd106b444
JackCokebb <reo2726@gmail.com>
2024-07-30 19:59:42 +0900

    fix: OAuth 검증 로직 - 전략패턴 적용

--- src/main/java/com/eod/sitree/auth/service/AuthService.java
+++ src/main/java/com/eod/sitree/auth/service/AuthService.java
@@ -6,10 +6,13 @@ import com.eod.sitree.auth.exception.AuthSettingException;
 import com.eod.sitree.auth.ui.dto.OAuthResponseDto;
 import com.eod.sitree.common.exception.ApplicationErrorType;
 import com.eod.sitree.member.domain.model.Provider;
+import jakarta.annotation.PostConstruct;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import lombok.Getter;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,20 +23,20 @@ public class AuthService {
 
     public static final KeyPair jwtKeypair = generateKeyPair();
     private final OAuthRepository oAuthRepository;
+    private final Map<Provider, Function<String, ResponseEntity<OAuthResponseDto>>> functionMap = new EnumMap<>(Provider.class);
+
+    @PostConstruct
+    public void init() {
+        functionMap.put(Provider.GOOGLE, this::validateGoogleToken);
+        functionMap.put(Provider.GITHUB, this::validateGithubToken);
+    }
 
     public void validateOauthToken(Provider provider, String token, String email) {
-        ResponseEntity<OAuthResponseDto> response = null;
+        ResponseEntity<OAuthResponseDto> response = functionMap.get(provider).apply(token);
+        validateTokenResponse(email, response);
+    }
 
-        if (Provider.GOOGLE.name().equalsIgnoreCase(provider.name())) {
-
-            response = oAuthRepository.validateGoogleToken(token);
-        }
-
-        if (Provider.GITHUB.name().equalsIgnoreCase(provider.name())) {
-
-            response = oAuthRepository.validateGithubToken(token);
-        }
-
+    private void validateTokenResponse(String email, ResponseEntity<OAuthResponseDto> response) {
         if (response == null || !response.getStatusCode().is2xxSuccessful()
             || response.getBody() == null || email.equals(response.getBody().getEmail())) {
 
@@ -51,6 +54,16 @@ public class AuthService {
 
             throw new AuthSettingException(ApplicationErrorType.AUTH_KEYPAIR_GENERATION_ERROR);
         }
+    }
+
+    private ResponseEntity<OAuthResponseDto> validateGoogleToken(String token) {
+
+        return oAuthRepository.validateGoogleToken(token);
+    }
+
+    private ResponseEntity<OAuthResponseDto> validateGithubToken(String token) {
+
+        return oAuthRepository.validateGithubToken(token);
     }
 
 }

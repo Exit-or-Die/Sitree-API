b3e1111331d4daa404b92a5d94118f1ec86e9031
SieunKiim <sieun960513@naver.com>
2024-08-20 00:01:59 +0900

    feat: 이미지 등록 API 완성

--- src/main/java/com/eod/sitree/image/ui/ImageController.java
+++ src/main/java/com/eod/sitree/image/ui/ImageController.java
@@ -1,12 +1,9 @@
 package com.eod.sitree.image.ui;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.eod.sitree.common.exception.ApplicationErrorType;
+import com.eod.sitree.auth.support.AuthNotRequired;
 import com.eod.sitree.common.response.ResponseDto;
-import java.io.IOException;
+import com.eod.sitree.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,24 +14,11 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/image")
 @RequiredArgsConstructor
 public class ImageController {
-    private final AmazonS3Client amazonS3Client;
 
-    @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    private final ImageService imageService;
 
     @PostMapping
     public ResponseDto<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        try {
-            String fileName=file.getOriginalFilename();
-            String fileUrl= "https://" + bucket + "/" +fileName;
-            ObjectMetadata metadata= new ObjectMetadata();
-            metadata.setContentType(file.getContentType());
-            metadata.setContentLength(file.getSize());
-            amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
-            return ResponseDto.ok(fileUrl);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return ResponseDto.error(ApplicationErrorType.IMAGE_UPLOAD_FAIL);
-        }
+        return ResponseDto.ok(imageService.uploadImage(file));
     }
 }

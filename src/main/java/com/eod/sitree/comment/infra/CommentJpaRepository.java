07fa7fdbdeaf3c4366afc6e7f8467cca4e5e234e
JackCokebb <reo2726@gmail.com>
2024-08-21 01:17:19 +0900

    feature: 댓글 조회 entityGraph 처리

--- src/main/java/com/eod/sitree/comment/infra/CommentJpaRepository.java
+++ src/main/java/com/eod/sitree/comment/infra/CommentJpaRepository.java
@@ -2,11 +2,13 @@ package com.eod.sitree.comment.infra;
 
 import com.eod.sitree.comment.infra.entity.CommentEntity;
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentJpaRepository extends JpaRepository<CommentEntity, Long> {
 
     CommentEntity findByCommentId(Long commentId);
 
+    @EntityGraph(attributePaths = {"childComments"})
     List<CommentEntity> findAllByProjectIdAndIsChildComment(Long projectId, Boolean isChildComment);
 }

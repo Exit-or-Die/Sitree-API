dd36af62600211dfaff8c2fe87e708699e47d448
JackCokebb <reo2726@gmail.com>
2024-08-25 21:12:27 +0900

    fix: baseEntity 날짜 사라지는 이슈 해경

--- src/main/java/com/eod/sitree/common/domain/model/BaseTimeDomain.java
+++ src/main/java/com/eod/sitree/common/domain/model/BaseTimeDomain.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public class BaseTimeDomain {
 
-    private final LocalDateTime createdAt;
+    private LocalDateTime createdAt;
 
     private LocalDateTime modifiedAt;
 

2f5a23dbbc830f17681245b0d645ac3e4cd0ce3a
SieunKiim <sieun960513@naver.com>
2024-11-10 16:58:36 +0900

    refactor: 페이징 요청 Dto 공통 class로 변경

--- src/main/java/com/eod/sitree/project/ui/dto/request/ProjectListRequestDto.java
+++ src/main/java/com/eod/sitree/project/ui/dto/request/ProjectListRequestDto.java
@@ -4,15 +4,17 @@ import static com.eod.sitree.comment.infra.entity.QCommentEntity.commentEntity;
 import static com.eod.sitree.project.infra.entity.QProjectEntity.projectEntity;
 import static com.eod.sitree.project.infra.entity.QProjectLikesEntity.projectLikesEntity;
 
+import com.eod.sitree.common.request.PageRequestDto;
 import com.querydsl.core.types.Expression;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@AllArgsConstructor
-public class ProjectListRequestDto {
-    private SortType sortType;
-    private int pageNo;
+@Setter
+@NoArgsConstructor
+public class ProjectListRequestDto extends PageRequestDto {
+    private SortType sortType = SortType.LATEST;
 
     @Getter
     public enum SortType {

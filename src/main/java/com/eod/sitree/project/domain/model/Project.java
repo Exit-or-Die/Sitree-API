abf30e9b1bbcdce88a1ca57f1aff9ddd25db87ba
SieunKiim <sieun960513@naver.com>
2024-12-10 20:25:18 +0900

    feat: 아키텍쳐 순서 맞춰서 노출 (성훈씨 희망)

--- src/main/java/com/eod/sitree/project/domain/model/Project.java
+++ src/main/java/com/eod/sitree/project/domain/model/Project.java
@@ -1,12 +1,14 @@
 package com.eod.sitree.project.domain.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
 @RequiredArgsConstructor
 public class Project {
     private final Head head;
@@ -15,6 +17,21 @@ public class Project {
     private final List<Techview> techviews;
     private final List<Architecture> architectures;
     private final List<Participant> participants;
+
+    public Project(Head head, List<Category> categories, Overview overview,
+            List<Techview> techviews,
+            List<Architecture> architectures, List<Participant> participants, Long viewCount) {
+        List<Architecture> arr = new ArrayList<>(architectures);
+        arr.sort(Comparator.comparingInt(o -> o.getArchitectureType().getTabNumber()));
+        this.head = head;
+        this.categories = categories;
+        this.overview = overview;
+        this.techviews = techviews;
+        this.architectures = arr;
+        this.participants = participants;
+        this.viewCount = viewCount;
+    }
+
     private Long viewCount = 0L;
 
     public String getHealthCheckUrl() {

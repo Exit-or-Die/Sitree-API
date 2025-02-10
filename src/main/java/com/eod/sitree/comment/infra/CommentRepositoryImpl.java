5a8025c32588cfc12a6f3790c74e1a340be020b1
JackCokebb <reo2726@gmail.com>
2025-02-11 01:19:02 +0900

    feature: comment 조회시 > 멤버 정보 반환

--- src/main/java/com/eod/sitree/comment/infra/CommentRepositoryImpl.java
+++ src/main/java/com/eod/sitree/comment/infra/CommentRepositoryImpl.java
@@ -7,9 +7,17 @@ import com.eod.sitree.comment.domain.model.CommentType;
 import com.eod.sitree.comment.domain.modelrepository.CommentRepository;
 import com.eod.sitree.comment.exception.CommentException;
 import com.eod.sitree.comment.infra.entity.CommentEntity;
+import com.eod.sitree.comment.infra.entity.QCommentEntity;
+import com.eod.sitree.comment.ui.dto.response.CommentResponseDto;
 import com.eod.sitree.common.exception.ApplicationErrorType;
 import com.eod.sitree.member.domain.model.Member;
 import com.eod.sitree.member.infra.entity.MemberEntity;
+import com.eod.sitree.member.infra.entity.QMemberEntity;
+import com.eod.sitree.project.infra.entity.QParticipantEntity;
+import com.eod.sitree.project.infra.entity.QProjectEntity;
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.ArrayList;
@@ -28,8 +36,9 @@ public class CommentRepositoryImpl extends QuerydslRepositorySupport implements 
     private final CommentJpaRepository commentJpaRepository;
     private final JPAQueryFactory jpaQueryFactory;
 
-    public CommentRepositoryImpl(CommentJpaRepository commentJpaRepository, JPAQueryFactory jpaQueryFactory) {
-        super(Member.class);
+    public CommentRepositoryImpl(CommentJpaRepository commentJpaRepository,
+        JPAQueryFactory jpaQueryFactory) {
+        super(Comment.class);
         this.jpaQueryFactory = jpaQueryFactory;
         this.commentJpaRepository = commentJpaRepository;
     }
@@ -49,7 +58,8 @@ public class CommentRepositoryImpl extends QuerydslRepositorySupport implements 
 
     @Override
     public Boolean existParentByParentCommentId(Long parentCommentId, CommentType commentType) {
-        return commentJpaRepository.findByCommentIdAndIsChildCommentAndCommentType(parentCommentId, false, commentType) != null;
+        return commentJpaRepository.findByCommentIdAndIsChildCommentAndCommentType(parentCommentId,
+            false, commentType) != null;
     }
 
     @Override
@@ -61,6 +71,52 @@ public class CommentRepositoryImpl extends QuerydslRepositorySupport implements 
             .orElseGet(ArrayList::new)
             .stream().map(CommentEntity::toDomainModel)
             .toList();
+    }
+
+    @Override
+    public List<CommentResponseDto> findChildCommentsByProjectIdAndParentCommentId(Long projectId,
+        Long parentCommentId) {
+
+        QMemberEntity memberEntity = new QMemberEntity("memberEntity");
+        QParticipantEntity participantEntity = new QParticipantEntity("participantEntity");
+
+        return jpaQueryFactory.select(
+                Projections.constructor(
+                    CommentResponseDto.class,
+                    commentEntity.commentId,
+                    commentEntity.commentType,
+                    commentEntity.targetId,
+                    commentEntity.contents,
+                    memberEntity.memberId,
+                    memberEntity.nickname,
+                    memberEntity.profileImgUrl,
+                    new CaseBuilder().when(participantEntity.isLeader.isNull())
+                        .then(false)
+                        .otherwise(participantEntity.isLeader),
+                    new CaseBuilder().when(participantEntity.participantId.isNull())
+                        .then(false)
+                        .otherwise(true),
+                    commentEntity.parentCommentId,
+                    commentEntity.isChildComment,
+                    commentEntity.isDeleted
+                )
+            )
+            .from(commentEntity)
+            .innerJoin(memberEntity)
+            .on(commentEntity.createMemberId.eq(memberEntity.memberId))
+            .leftJoin(participantEntity)
+            .on(
+                participantEntity.projectId.eq(commentEntity.targetId),
+                participantEntity.memberId.eq(commentEntity.createMemberId)
+            )
+            .where(
+                commentEntity.commentType.eq(CommentType.PROJECT),
+                commentEntity.targetId.eq(projectId),
+                commentEntity.parentCommentId.eq(parentCommentId),
+                commentEntity.isChildComment.eq(true),
+                commentEntity.isDeleted.eq(false)
+            )
+            .fetch();
     }
 
     @Override
@@ -77,6 +133,58 @@ public class CommentRepositoryImpl extends QuerydslRepositorySupport implements 
 
         return new PageImpl<>(
             query.fetch().stream().map(CommentEntity::toDomainModel).toList(),
+            pageable,
+            query.fetchCount()
+        );
+    }
+
+    @Override
+    public Page<CommentResponseDto> findDtoByProjectIdAsPage(Long projectId, Pageable pageable) {
+
+        QMemberEntity memberEntity = new QMemberEntity("memberEntity");
+        QParticipantEntity participantEntity = new QParticipantEntity("participantEntity");
+
+        JPQLQuery<CommentResponseDto> query = getQuerydsl().applyPagination(
+            pageable,
+            jpaQueryFactory.select(
+                    Projections.constructor(
+                        CommentResponseDto.class,
+                        commentEntity.commentId,
+                        commentEntity.commentType,
+                        commentEntity.targetId,
+                        commentEntity.contents,
+                        memberEntity.memberId,
+                        memberEntity.nickname,
+                        memberEntity.profileImgUrl,
+                        new CaseBuilder().when(participantEntity.isLeader.isNull())
+                            .then(false)
+                            .otherwise(participantEntity.isLeader),
+                        new CaseBuilder().when(participantEntity.participantId.isNull())
+                            .then(false)
+                            .otherwise(true),
+                        commentEntity.parentCommentId,
+                        commentEntity.isChildComment,
+                        commentEntity.isDeleted
+                    )
+                )
+                .from(commentEntity)
+                .innerJoin(memberEntity)
+                .on(commentEntity.createMemberId.eq(memberEntity.memberId))
+                .leftJoin(participantEntity)
+                .on(
+                    participantEntity.projectId.eq(commentEntity.targetId),
+                    participantEntity.memberId.eq(commentEntity.createMemberId)
+                )
+                .where(
+                    commentEntity.commentType.eq(CommentType.PROJECT),
+                    commentEntity.targetId.eq(projectId),
+                    commentEntity.isChildComment.eq(false),
+                    commentEntity.isDeleted.eq(false)
+                )
+        );
+
+        return new PageImpl<>(
+            query.fetch(),
             pageable,
             query.fetchCount()
         );
